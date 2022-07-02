66514d757ec0713843a234a957553dcc985b30a0
Guipas68 <58084848+Nausicaa68@users.noreply.github.com>
2022-07-02 14:03:57 +0200

    delete files

--- nucleotidecounter.py
+++
@@ -1,8 +0,0 @@
-def countLetters(dna) :
-    letters={'A':0, 'T':0, 'G':0, 'C':0}
-    for c in dna :
-        letters[c]+=1
-    return letters
-    
-if __name__ == '__main__':
-    print(countLetters("ATGCAATTGGCCA"))
